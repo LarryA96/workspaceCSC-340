b0da90f495a79a6703479e3ae492796a32dc8b6f
Larry Alston <larryalston3694@gmail.com>
2024-08-31 03:31:54 -0400

    first commit

---
+++ Week1/js/main.js
@@ -0,0 +1,20 @@
+const ball = document.getElementById("ball");
+document.addEventListener("keydown", handleKeyPress);
+let position = 0;
+
+function handleKeyPress(e) {
+  if (e.code === "ArrowLeft") {
+    position = position - 10;
+  }
+  if (e.code === "ArrowRight") {
+    position = position + 10;
+  }
+  if (position < 0) {
+    position = 0;
+  }
+  refresh();
+}
+function refresh() {
+  ball.link.left = position + "px";
+  console.log("New position is " + position + "px");
+}
