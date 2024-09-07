9d12bc6f7bb03d18bf3af48f2578a3518e85ceb5
Larry Alston <larryalston3694@gmail.com>
2024-09-07 04:32:38 -0400

    Writing a list to HTML page using JavaScript

---
+++ Week2/js/main.js
@@ -0,0 +1,35 @@
+/*
+Name: Larry Alston
+Due Date: September 8, 2024
+Assignment: Week 2 project "Favorite Foods"
+*/
+
+//Write heading to the page
+document.getElementById("heading").innerHTML = "Favorite Foods";
+
+//Create unordered list and give it an id
+let list = document.createElement("ul");
+list.setAttribute("id", "list");
+
+//Add unordered list to the body of webpage
+document.getElementById("body").append(list);
+
+//Style list so that it is centered
+list.style.margin = "auto";
+list.style.width = "fit-content";
+
+//Create array of favorite foods
+let favFoods = [
+  "Cheeseburger",
+  "Shrimp alfredo",
+  "Spaghetti",
+  "Salmon",
+  "Potato",
+];
+
+//Add favorite foods to list
+for (let item of favFoods) {
+  let newItem = document.createElement("li");
+  newItem.innerHTML = item;
+  list.append(newItem);
+}
